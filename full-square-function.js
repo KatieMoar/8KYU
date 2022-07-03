426508ebf88b0c6b6d2d4a53a7e4bf051a8d7cf3
Katie Moar <katiemoar@hotmail.com>
2022-07-02 19:17:42 -0500

    addition: full-square-function.js

---
+++ full-square-function.js
@@ -0,0 +1,13 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function square(x) {
+    return Math.pow(x, 2)
+  }
+  
+// OR
+
+const square = x => Math.pow(x, 2)
