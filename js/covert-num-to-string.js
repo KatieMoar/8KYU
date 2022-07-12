4b5dc3d16b4494ea4e42134a3098aa68379537be
Katie Moar <katiemoar@hotmail.com>
2022-07-12 18:48:19 -0500

    addition: covert-to-string.js

---
+++ js/covert-num-to-string.js
@@ -0,0 +1,25 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+
+function numberToString(num) {
+    return num.toString()
+  }
+
+/* -------------------------------------------- */
+/*                     NOTES                    */
+/* -------------------------------------------- */
+
+//Finished really quickly! Woo!
