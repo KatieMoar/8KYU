98197776cad4754ebb99a6222b6a6eee4a1259b6
Katie Moar <katiemoar@hotmail.com>
2022-07-16 20:59:57 -0500

    addition: even-or-odd.js

---
+++ js/even-or-odd.js
@@ -0,0 +1,16 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+
+function even_or_odd(number) {
+    console.log((number % 2 === 0) ? true : false)
+}
+
+even_or_odd(2)
