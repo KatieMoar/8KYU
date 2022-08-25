55f3800c0342a8f3d9402755ff09b88fdc41890d
Katie Moar <katiemoar@hotmail.com>
2022-08-25 16:09:11 -0500

    addition: super-duper-easy.js

---
+++ js/super-duper-easy.js
@@ -0,0 +1,18 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// Make a function that returns the value multiplied by 50 and increased by 6. 
+// If the value entered is a string it should return "Error".
+
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+
+function problem(x){
+    return typeof x === "number" ? x * 50 + 6 : "Error";
+  }
+
+//   problem("hello") // "Error"
+  problem(1), 56
