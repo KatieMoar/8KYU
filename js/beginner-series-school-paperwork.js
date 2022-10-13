4279c18d409c231a9502d12764c6034d4da96ec8
Katie Moar <katiemoar@hotmail.com>
2022-10-12 19:46:46 -0500

    addition: beginner-series-school-paperwork.js

---
+++ js/beginner-series-school-paperwork.js
@@ -0,0 +1,35 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+// Waiting for translations and Feedback! Thanks!
+
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+
+function paperwork(n, m) {
+    if(m < 0 || n < 0){
+        return 0
+    }
+    return m * n
+}
+
+
+/* -------------------------------------------- */
+/*                  TEST CASES                  */
+/* -------------------------------------------- */
+
+paperwork(5,5)// 25, 'Failed at paperwork(5, 5)'
+paperwork(5,-5)// 0, 'Failed at paperwork(5, -5)'
+paperwork(-5,-5)// 0, 'Failed at paperwork(-5, -5)'
+paperwork(-5,5)// 0, 'Failed at paperwork(-5, 5)'
+paperwork(5,0)//, 0, 'Failed at paperwork(5, 0)'
