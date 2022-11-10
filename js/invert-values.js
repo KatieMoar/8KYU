c431aa2b31643e5b58e49cda2e18a9e9bb41db82
Katie Moar <katiemoar@hotmail.com>
2022-11-09 18:08:02 -0600

    correction

--- js/invert-values.js
+++ js/invert-values.js
@@ -27,6 +27,15 @@ function invert(array) {
  }
 
 
+ function invert(array) {
+    var newArr = [];
+    for(var i = 0; i < array.length; i++){
+      newArr.push(-array[i]);
+    }
+     return newArr;
+  }
+
+
 /* -------------------------------------------- */
 /*                  TEST CASES                  */
 /* -------------------------------------------- */
